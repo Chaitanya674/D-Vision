b3afa7c72c412611f15acc8636f740572fd5d6fd
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-05-18 12:52:32 +0530

    D-vision

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import io
 import math 
 from flask import Flask, request , send_from_directory , jsonify
 from werkzeug.utils import secure_filename
-from flask_cors import CORS 
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
